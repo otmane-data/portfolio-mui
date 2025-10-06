24422fcaadd34861b50fece793ea857d985fa886
otmane-data <aghzarotmane@gmail.com>
2025-10-06 14:04:08 +0100

    osman n9s mn vibe coding

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // IMPORTANT: GitHub Pages project site base path
-    // This makes built asset URLs like /portfolio-mui/...
-  base:  '/portfolio-mui',
   plugins: [react()],
 })
